31423d0475b322fb116e2bc02edc21173192c75a
Bobby Low <lyouliang@gmail.com>
2013-08-23 15:44:18 -0400

    Added menu items

--- src/com/bobbylow/TomatoCat/TomatoCatActivity.java
+++ src/com/bobbylow/TomatoCat/TomatoCatActivity.java
@@ -2,8 +2,12 @@ package com.bobbylow.TomatoCat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
-public class TomatoCatActivity extends Activity {
+public class TomatoCatActivity extends SherlockFragmentActivity {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +15,26 @@ public class TomatoCatActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.options, menu);
+        return(super.onCreateOptionsMenu(menu));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                return(true);
+            case R.id.about:
+                return(true);
+            case R.id.preferences:
+                return(true);
+            case R.id.help:
+                return(true);
+        }
+        return(super.onOptionsItemSelected(item));
     }
 }
